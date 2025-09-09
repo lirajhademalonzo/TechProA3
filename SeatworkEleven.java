46962dee77457a7f9f3078135de51d5bb2b05cee
lirajhademalonzo <lirajhademalonzo@gmail.com>
2025-09-09 10:09:39 +0000

    java

--- SeatworkEleven.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main (String[] args) {
-        System.out.println("SEATWORKELEVENPART1");
-        int[][] myNumbers = { {1, 4, 2}}
-    }
-}
---
+++ Seatworkelevenp1.java
@@ -0,0 +1,9 @@
+public class Seatworkelevenp1 {
+    public static void main (String[] args) {
+        System.out.println("SEATWORKELEVENPART1");
+        int[][] myNumbers = { {1, 4, 2}, {3,6,8} };
+        myNumbers[1][2]=9;
+
+        System.out.println(myNumbers[1][2]);
+    }
+}
