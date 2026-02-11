22251e1a2f8900693a7dd560d2eaf012cd3fe713
lirajhademalonzo <lirajhademalonzo@gmail.com>
2026-02-11 09:11:42 +0000

    java

---
+++ Task7p2sm2.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Task7p2sm2 implements Runnable {
+
+    private final LocalTime alarmTime;
+
+    public Task7p2sm2 (LocalTime alarmTime) {
+        this.alarmTime = alarmTime;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Countdown started...");
+
+        while (true) {
+            LocalTime now = LocalTime.now();
+            Duration remaining = Duration.between(now, alarmTime);
+
+            if (!remaining.isPositive() || remaining.isZero()) {
+                break;
+            }
+
+            long hours = remaining.toHours();
+            long minutes = remaining.toMinutes() % 60;
+            long seconds = remaining.getSeconds() % 60;
+
+
+            System.out.printf("%02d:%02d:%02d remaining%n", hours, minutes, seconds);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted!");
+                return;
+            }
+        }
+
+        System.out.println("\n*ALARM NOISES*");
+        Toolkit.getDefaultToolkit().beep();
+        System.out.println("ALARM! Press Enter to stop...");
+    }
---
+++ Task7sm2.java
@@ -0,0 +1,33 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Task7sm2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Please use HH:MM:SS");
+            }
+        }
+
+
+        Thread alarmThread = new Thread(new AlarmClock(alarmTime));
+        alarmThread.start();
+
+
+        scanner.nextLine();
+        System.out.println("Program ended.");
+        scanner.close();
+    }
+}
