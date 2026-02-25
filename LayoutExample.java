d9203b9858ea06d978f7c1b88a6e79dc4a3153bc
lirajhademalonzo <lirajhademalonzo@gmail.com>
2026-02-25 02:05:51 +0000

    s2

---
+++ LayoutExample.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class LayoutExample {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Absulutely Positioning");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+
+        frame.setLayout(null);
+
+        JButton Btn1 = new JButton("Button 1");
+        Btn1.setBounds(50,50,150,40);
+
+        JButton Btn2 = new JButton("Button 2");
+        Btn2.setBounds(150,120,150,40);
+
+        frame.add(Btn1);
+        frame.add(Btn2);
+
+        frame.setVisible(true);
+    }
+}
